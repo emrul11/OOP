59906e27d08c5312331803c2ead72e550c3cb097
emrul11 <emrulkabir111@gmail.com>
2026-01-28 17:06:23 +0600

    Added csv files

--- OOPPractice/Basics/Program.cs
+++ OOPPractice/Basics/Program.cs
@@ -1,28 +1,84 @@
 ﻿using Basics;
 
-Student student1 = new Student();
-student1.name = "Alice";
-student1.email = "Alice@gmail.com";
-student1.contactNumber = "1234567";
+//Student student1 = new Student();
+//student1.name = "Alice";
+//student1.email = "Alice@gmail.com";
+//student1.contactNumber = "1234567";
 
-Student student2 = new Student();
-student2.name = "Bob";
-student2.email = "Bob@gmail.com";
-student2.contactNumber = "87654321";
+//Student student2 = new Student();
+//student2.name = "Bob";
+//student2.email = "Bob@gmail.com";
+//student2.contactNumber = "87654321";
 
 
-Result result1 = new Result();
-result1.physics = 90;
-result1.chemistry = 75;
-result1.biology = 85;
+//Result result1 = new Result();
+//result1.physics = 90;
+//result1.chemistry = 75;
+//result1.biology = 85;
 
-Result result2 = new Result();
-result2.physics = 100;
-result2.chemistry = 80;
-result2.biology = 55;
+//Result result2 = new Result();
+//result2.physics = 100;
+//result2.chemistry = 80;
+//result2.biology = 55;
 
-student1.result = result1;
-student2.result = result2;
+//student1.result = result1;
+//student2.result = result2;
+
+
+Console.WriteLine("1. Add Student");
+Console.WriteLine("2. Show All Students");
+Console.Write("Choose option: ");
+int choice = int.Parse(Console.ReadLine());
+
+if (choice == 1)
+{
+    Student student = CreateStudentFromInput();
+    //repository.Save(student);
+    Console.WriteLine("Student saved successfully.");
+}
+else if (choice == 2)
+{
+   // List<Student> students = repository.GetAllStudents();
+    //foreach (Student student in students)
+    //{
+    //    DisplayStudentInfo(student);
+    //    Console.WriteLine("----------------------------");
+    //}
+}
+else
+{
+    Console.WriteLine("Invalid choice");
+}
+    
+
+    static Student CreateStudentFromInput()
+{
+    Student student = new Student();
+
+    Console.Write("Name: ");
+    student.name = Console.ReadLine();
+
+    Console.Write("Email: ");
+    student.email = Console.ReadLine();
+
+    Console.Write("Contact Number: ");
+    student.contactNumber = Console.ReadLine();
+
+    Result result = new Result();
+
+    Console.Write("Physics: ");
+    result.physics = double.Parse(Console.ReadLine());
+
+    Console.Write("Chemistry: ");
+    result.chemistry = double.Parse(Console.ReadLine());
+
+    Console.Write("Biology: ");
+    result.biology = double.Parse(Console.ReadLine());
+
+    student.result = result;
+
+    return student;
+}
 
 void DisplayStudentInfo(Student student) { 
     Console.WriteLine("Name: " +student.name);
@@ -36,7 +92,7 @@ void DisplayStudentInfo(Student student) {
 }
 
 
-DisplayStudentInfo(student1 );
-Console.WriteLine("_________________________\n");
-DisplayStudentInfo(student2);
+//DisplayStudentInfo(student1 );
+//Console.WriteLine("_________________________\n");
+//DisplayStudentInfo(student2);
 

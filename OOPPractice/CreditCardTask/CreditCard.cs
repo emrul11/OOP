204afa0f12310b6f0a4d562276683cb9598245d5
emrul11 <emrulkabir111@gmail.com>
2026-02-01 10:42:29 +0600

    CreditCard project created, CreditCard.cs started

---
+++ OOPPractice/CreditCardTask/CreditCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreditCardTask
+{
+    public class CreditCard
+    {
+        private const decimal DefaultCreditLimit = 500000;
+        private const decimal DefaultCashWithdrawalLimit = 50000;
+
+        public string CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public string BankName { get; set; }
+
+        public decimal CreditLimit {  get; set; }
+        public decimal CashWithdrawalLimit { get; set; }
+
+    }
+}
